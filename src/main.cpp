2f8089a7de1eead51a240cb72a252e856af99dc1
Kon Hyong Kim <konhyong@gmail.com>
2022-09-20 12:05:56 -0700

    minor longitude adjustment

--- src/main.cpp
+++ src/main.cpp
@@ -254,7 +254,7 @@ struct SensoriumApp : public DistributedAppWithState<State> {
       sourceGeoLoc.lon = lon.get();
       sourceGeoLoc.radius = radius.get();
       targetGeoLoc.lat = 0;
-      targetGeoLoc.lon = 150;
+      targetGeoLoc.lon = -80;
       targetGeoLoc.radius = 0.01;
       morphProgress = morphDuration;
       hoverDuration = 0;
