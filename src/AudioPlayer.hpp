9cd1b69b86a6686a2b92b3bf4c71466e2f2baa81
Timothy Wood <fishuyo@gmail.com>
2024-09-12 12:48:23 -0700

    add intro audio, change spatialization simple, fix sub channel

--- src/AudioPlayer.hpp
+++ src/AudioPlayer.hpp
@@ -9,7 +9,10 @@ namespace al {
 struct AudioPlayer {
 
   SoundFilePlayerTS audioPlayerTS;
+  SoundFilePlayerTS intro;
   std::vector<float> buffer;
+  std::vector<float> buffer2;
+  ParameterBool playIntro{"Play_Intro", "", 0};
   ParameterBool playAudio{"Play_Audio", "", 0};
   ParameterBool playLoop{"Loop Audio", "", 1};
   ParameterBool pauseAudio{"Pause Audio", "", 0};
@@ -31,15 +34,26 @@ struct AudioPlayer {
               << std::endl;
     audioPlayerTS.setPause();
     audioPlayerTS.setLoop();
+
+    if (!intro.open("data/Sketches.wav")) {
+      std::cerr << "File not found: " << "data/Sketches.wav" << std::endl;
+    }
+    intro.setPause();
+    intro.setNoLoop();
+
   }
   void registerParams(ControlGUI *gui, PresetHandler &presets,
                       PresetSequencer &seq, State &state) {
-    *gui << playAudio << playLoop << pauseAudio << rewindAudio << AudioVolume
+    *gui << playIntro << playAudio << playLoop << pauseAudio << rewindAudio << AudioVolume
          << SubVolume;
 
     presets << AudioVolume << SubVolume << playAudio << rewindAudio;
-    seq << playAudio << rewindAudio << AudioVolume << SubVolume;
+    seq << playIntro << playAudio << rewindAudio << AudioVolume << SubVolume;
 
+    playIntro.registerChangeCallback([&](float value) {
+      if (value == 1.0)
+        intro.setPlay();
+    });
     playAudio.registerChangeCallback([&](float value) {
       if (value == 1.0)
         audioPlayerTS.setPlay();
@@ -68,25 +82,41 @@ struct AudioPlayer {
     if ((int)buffer.size() < bufferLength) {
       buffer.resize(bufferLength);
     }
+    if ((int)buffer2.size() < frames) {
+      buffer2.resize(frames);
+    }
     audioPlayerTS.getFrames(frames, buffer.data(), (int)buffer.size());
+    intro.getFrames(frames, buffer2.data(), (int)buffer2.size());
     int second = (channels < 2) ? 0 : 1;
+
     while (io()) {
       int frame = (int)io.frame();
       int idx = frame * channels;
       float l = buffer[idx];
       float r = buffer[idx + second];
+      float s = buffer2[frame];
 
       if (al::sphere::isSphereMachine()) {
-        for (int i = 1; i <= 12; i++)
+        for (int i = 0; i < 12; i++)
+          io.out(i) = (s+l) * AudioVolume * 0.4;
+
+        for (int i = 16; i < 31; i++)
           io.out(i) = l * AudioVolume * 0.4;
-        io.out(24) = l * AudioVolume * 0.7;
+
+        io.out(24) = s * AudioVolume * 0.8;
+
+        for (int i = 31; i < 46; i++)
+          io.out(i) = r * AudioVolume * 0.4;
+
         for (int i = 49; i <= 60; i++)
           io.out(i) = r * AudioVolume * 0.4;
-        io.out(39) = r * AudioVolume * 0.7;
-        io.out(48) = (l + r) * SubVolume * 0.5;
+
+        io.out(39) = s * AudioVolume * 0.8;
+        // io.out(48) = (l + r) * SubVolume * 0.5;
+        io.out(47) = (l + r) * SubVolume * 0.5;
       } else {
-        io.out(0) = l * AudioVolume;
-        io.out(1) = r * AudioVolume;
+        io.out(0) = (s+l) * AudioVolume;
+        io.out(1) = (s+r) * AudioVolume;
       }
     }
   }
